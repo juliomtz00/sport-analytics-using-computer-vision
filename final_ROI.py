74fc639034563e8799ed2f8696d7acdfcd510989
jen-toj <133147287+jen-toj@users.noreply.github.com>
2023-05-15 03:11:18 +0200

    Add files via upload

--- final_ROI.py
+++ final_ROI.py
@@ -13,9 +13,6 @@ import argparse
 import numpy as np
 
 
-# Initialize list
-coord_list = list()
-field_done = 0
 # Get image coordinates
 def get_pixel_coordinates(event, x, y, flags, param):
     """
@@ -57,6 +54,14 @@ def compute_line_segments(NPOINTS):
 # ------------------ Define and initialise variables ---------------------- #
 max_value = 255
 max_value_H = 360 // 2
+coord_list = list()
+field_done = 0
+low_H = 1
+low_S = 0
+low_V = 0
+high_H = 19
+high_S = 255
+high_V = 255
 window_capture_name = 'Input video'
 window_detection_name = 'Object Detection'
 
@@ -90,7 +95,6 @@ while True:
 
     NPOINTS = len(coord_list)
     # compute_line_segments(NPOINTS)
-    print(frame.shape)
     if field_done == 0:
         # Convert the list of coordinates to NumPy array
         points = np.array(coord_list, dtype=np.int32)
@@ -102,7 +106,7 @@ while True:
         cv2.polylines(frame, [points], isClosed=True, color=(0, 255, 0), thickness=2)
         mask_field = np.zeros(frame.shape, dtype=np.uint8)
         cv2.fillPoly(mask_field, [points], color=(0, 255, 0))
-        bitwise_AND3 = cv2.bitwise_and(frame_HSV, mask_field)
+        bitwise_AND4 = cv2.bitwise_and(bitwise_AND3, mask_field)
 
     # Convert the current frame from BGR to HSV
     frame_HLS = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS)
@@ -111,25 +115,42 @@ while True:
     frame_threshold[0:70,:]=0
     frame_threshold[670:-1, :] = 0
     # Filter out the grassy region from current frame and keep the moving object only
-
     bitwise_AND = cv2.bitwise_and(frame, frame, mask=frame_threshold)
-
     frame_HSV = cv2.cvtColor(bitwise_AND, cv2.COLOR_BGR2HSV)
-
     #frame_HSV = cv2.medianBlur(frame_HSV, 3)
     # Apply a threshold to the HSV image
     frame_threshold2 = cv2.inRange(frame_HSV, (40, 0, 0), (180, 255, 255))
-
-
+    frame_threshold3 = frame_threshold - frame_threshold2
     # Filter out the grassy region from current frame and keep the moving object only
-    bitwise_AND2 = cv2.bitwise_and(frame, bitwise_AND, mask=frame_threshold2)
+    bitwise_AND3 = cv2.bitwise_and(bitwise_AND, bitwise_AND, mask=frame_threshold3)
+    frame_HLS2 = cv2.cvtColor(bitwise_AND3, cv2.COLOR_BGR2HLS)
+    frame_threshold4 = cv2.inRange(frame_HLS2, (low_H, low_S, low_V), (high_H, high_S, high_V))
+    # frame_threshold4 = cv2.medianBlur(frame_threshold4, 3)
+    bitwise_AND3 = cv2.bitwise_and(bitwise_AND, bitwise_AND, mask=frame_threshold4)
 
+    if field_done:
+        bitwise_AND4 = cv2.cvtColor(bitwise_AND4, cv2.COLOR_RGB2GRAY)
+        # Detección de bordes con Canny
+        edges = cv2.Canny(bitwise_AND4, 15, 50)
+        if (np.count_nonzero(sum(edges)) > 0):
+            # Detectar los círculos en la imagen
+            circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 20, param1=100, param2=1, minRadius=5,
+                                       maxRadius=10)
+
+            # print(circles)
+            # Convertir las coordenadas y los radios a enteros
+            circles = np.uint16(np.around(circles))
+            # Dibujar los círculos encontrados en la imagen original
+            for i in circles[0, :]:
+                # Dibujar el círculo exterior
+                cv2.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 2)
+                # Dibujar el centro del círculo
+                cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 255), 3)
+        cv2.imshow(window_detection_name, bitwise_AND4)
+    else:
+        cv2.imshow(window_detection_name, bitwise_AND3)
     # Visualise both the input video and the object detection windows
     cv2.imshow(window_capture_name, frame)
-    if field_done:
-        cv2.imshow(window_detection_name, bitwise_AND3)
-    else:
-        cv2.imshow(window_detection_name, frame_HSV)
 
     # The program finishes if the key 'q' is pressed
     key = cv2.waitKey(5)
